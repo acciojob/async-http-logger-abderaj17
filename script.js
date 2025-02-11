ea6dbe491ccd9db1cd1eab6272c93b7d5fa526f2
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-11 19:24:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 const asyncFun = async ()=>{
 	const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-	const data = response.json();
+	const data = await response.json();
 	console.log(data);
 }
 asyncFun();
